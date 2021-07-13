011cd2c9fc3848e0db3134eebbf0d5e61bae0c04
Wilmer <terabytesoftw@gmail.com>
2021-07-13 12:06:08 -0400

    Add config aliases.

---
+++ config/params.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'yiisoft/aliases' => [
+        'aliases' => [
+            '@assetTailwind' => '@vendor/yii-extension/asset-tailwind',
+        ],
+    ],
+];
