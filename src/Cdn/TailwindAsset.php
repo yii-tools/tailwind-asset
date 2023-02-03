73abda15fe1ddfe032c516fd80966d8ad8687cd9
Wilmer Arambula <42547589+terabytesoftw@users.noreply.github.com>
2023-02-03 10:38:26 -0300

    Fix link cdn. (#24)

--- src/Cdn/TailwindAsset.php
+++ src/Cdn/TailwindAsset.php
@@ -9,5 +9,5 @@ use Yiisoft\Assets\AssetBundle;
 final class TailwindAsset extends AssetBundle
 {
     public bool $cdn = true;
-    public array $css = ['https://cdn.jsdelivr.net/npm/tailwindcss@3.2.4/dist/tailwind.min.css'];
+    public array $css = ['https://cdn.jsdelivr.net/npm/tailwindcss/dist/tailwind.min.css'];
 }
